c5bcb8682bc82765dabbea5f22f8cb0ed084bd18
Stanisław Chmiela <sjchmiela@gmail.com>
2020-01-07 17:48:32 +0100

    [test-suite] Add token emitting test to NewNotifications

--- apps/test-suite/tests/NewNotifications.js
+++ apps/test-suite/tests/NewNotifications.js
@@ -4,6 +4,7 @@ import { Platform } from '@unimodules/core';
 import * as Notifications from 'expo-notifications';
 
 import * as TestUtils from '../TestUtils';
+import { waitFor } from './helpers';
 
 export const name = 'expo-notifications';
 
@@ -13,17 +14,48 @@ export async function test(t) {
 
   describeWithPermissions('expo-notifications', () => {
     t.describe('getDevicePushTokenAsync', () => {
+      let subscription = null;
+      let event = null;
+
+      t.beforeAll(() => {
+        subscription = Notifications.addTokenListener(newEvent => {
+          event = newEvent;
+        });
+      });
+
+      t.afterAll(() => {
+        if (subscription) {
+          subscription.remove();
+          subscription = null;
+        }
+      });
+
       if (Platform.OS === 'android' || Platform.OS === 'ios') {
+        let token = null;
         t.it('resolves with a string', async () => {
           const devicePushToken = await Notifications.getDevicePushTokenAsync();
-          t.expect(devicePushToken.data).toBe('string');
+          t.expect(typeof devicePushToken.data).toBe('string');
+          token = devicePushToken;
+        });
+
+        t.it('emits an event with token', async () => {
+          await waitFor(500);
+          t.expect(event).toEqual(token);
         });
       }
 
       if (Platform.OS === 'web') {
+        let token = null;
+
         t.it('resolves with an object', async () => {
           const devicePushToken = await Notifications.getDevicePushTokenAsync();
-          t.expect(devicePushToken.data).toBe('object');
+          t.expect(typeof devicePushToken.data).toBe('object');
+          token = devicePushToken;
+        });
+
+        t.it('emits an event with token', async () => {
+          await waitFor(500);
+          t.expect(event).toEqual(token);
         });
       }
     });
